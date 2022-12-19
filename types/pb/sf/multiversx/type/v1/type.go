f7cfa9a137247fdf939430b25fd15e17f6098a2a
Elrond/ <marius.ciucam@gmail.com>
2022-12-19 13:47:33 +0200

    FIX: Push type.go

--- types/pb/sf/multiversx/type/v1/type.go
+++ types/pb/sf/multiversx/type/v1/type.go
@@ -4,18 +4,18 @@ import (
 	"time"
 )
 
-func (b *Block) ID() string {
+func (b *WrappedMultiversxBlock) ID() string {
 	return b.Hash
 }
 
-func (b *Block) Number() uint64 {
+func (b *WrappedMultiversxBlock) Number() uint64 {
 	return b.Height
 }
 
-func (b *Block) PreviousID() string {
+func (b *WrappedMultiversxBlock) PreviousID() string {
 	return b.PrevHash
 }
 
-func (b *Block) Time() time.Time {
+func (b *WrappedMultiversxBlock) Time() time.Time {
 	return time.Unix(0, int64(b.Timestamp)).UTC()
 }
