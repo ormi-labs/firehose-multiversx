1366a615a8517749369d13fa168f5c91b6dd5326
ssd04 <dariussuirab@gmail.com>
2024-04-02 16:55:26 +0300

    added block proto struct - rust setup

--- substreams-example/src/pb/pbmultiversx.rs
+++ substreams-example/src/pb/pbmultiversx.rs
@@ -1,6 +1,32 @@
 // @generated
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
+pub struct BlockHeader {
+    #[prost(uint64, tag="1")]
+    pub height: u64,
+    #[prost(string, tag="2")]
+    pub hash: ::prost::alloc::string::String,
+    #[prost(uint64, optional, tag="3")]
+    pub previous_num: ::core::option::Option<u64>,
+    #[prost(string, optional, tag="4")]
+    pub previous_hash: ::core::option::Option<::prost::alloc::string::String>,
+    #[prost(uint64, tag="5")]
+    pub final_num: u64,
+    #[prost(string, tag="6")]
+    pub final_hash: ::prost::alloc::string::String,
+    #[prost(uint64, tag="7")]
+    pub timestamp: u64,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Block {
+    #[prost(message, optional, tag="1")]
+    pub header: ::core::option::Option<BlockHeader>,
+    #[prost(message, optional, tag="2")]
+    pub multiversx_block: ::core::option::Option<OutportBlock>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct OutportBlock {
     #[prost(uint32, tag="1")]
     pub shard_id: u32,
